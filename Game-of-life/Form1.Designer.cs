5fdb0e95e3464062b694e8adcb17ac359cffaa9a
Muhammad Ahfas An Nahri <avastbrr@gmail.com>
2017-12-16 13:48:32 +0100

    Add project files.

---
+++ Game-of-life/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Game_of_life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.geneBox = new System.Windows.Forms.TextBox();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(77, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // restartButton
+            // 
+            this.restartButton.Enabled = false;
+            this.restartButton.Location = new System.Drawing.Point(283, 12);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(60, 23);
+            this.restartButton.TabIndex = 2;
+            this.restartButton.Text = "Restart";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // geneBox
+            // 
+            this.geneBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.geneBox.Location = new System.Drawing.Point(241, 13);
+            this.geneBox.Name = "geneBox";
+            this.geneBox.ReadOnly = true;
+            this.geneBox.Size = new System.Drawing.Size(36, 20);
+            this.geneBox.TabIndex = 2;
+            this.geneBox.TabStop = false;
+            this.geneBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.geneBox.TextChanged += new System.EventHandler(this.geneBox_TextChanged);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.mainPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainPanel.Location = new System.Drawing.Point(13, 43);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(460, 320);
+            this.mainPanel.TabIndex = 3;
+            this.mainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.mainPanel_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Generation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(321, 365);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "AN NAHRI Muhammad Ahfas";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Populate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(394, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Set Interval";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(349, 14);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(39, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 384);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.geneBox);
+            this.Controls.Add(this.startButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Conway\'s Game of Life";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.TextBox geneBox;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Game-of-life/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_of_life
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static Random GetRandom = new Random();
+        static int res = 10;
+        static int cols = 460 / res;
+        static int rows = 320 / res;
+        static int off = 0;
+        static int generation = 0;
+        static int[] moreZero = { 0, 0, 1 };
+
+        public static int[,] grid = make2Darray(cols, rows);
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 50;
+            numericUpDown1.Value = timer1.Interval / 10;
+            geneBox.Text = $"{generation}";
+            
+        }
+
+        private void mainPanel_Paint(object sender, PaintEventArgs e)
+        {
+            //Graphics graphics = mainPanel.CreateGraphics();
+            //Pen pBlack = new Pen(Color.Black, 1);
+            //for (int i = 0; i < cols; i++)
+            //{
+            //    for (int j = 0; j < rows; j++)
+            //    {
+            //        int x = i * res;
+            //        int y = j * res;
+                    
+            //        graphics.DrawRectangle(pBlack, new Rectangle(x, y, res - off, res - off));
+            //    }
+            //}
+        }
+        
+        // Functions
+        static int[,] make2Darray(int cols, int rows)
+        {
+            int[,] array = new int[cols,rows];
+            return array;
+        }
+        static void updateDraw(Control obj)
+        {
+            Graphics graphics = obj.CreateGraphics();
+            //Pen pBlack = new Pen(Color.Black, 1);
+            SolidBrush bBlack = new SolidBrush(Color.Black),
+                       bWhite = new SolidBrush(Color.White); ;
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    int x = i * res;
+                    int y = j * res;
+
+                    if (grid[i, j] == 1)
+                        graphics.FillRectangle(bBlack, new Rectangle(x, y, res - off, res - off));
+                    else
+                        graphics.FillRectangle(bWhite, new Rectangle(x, y, res - off, res - off));
+
+                    //graphics.DrawRectangle(pBlack, new Rectangle(x, y, res - off, res - off));
+                }
+            }
+            graphics.Dispose();
+            //pBlack.Dispose();
+            bBlack.Dispose();
+            bWhite.Dispose();
+        }
+
+        static void beginGrowth(Control obj)
+        {
+            Graphics graphics = obj.CreateGraphics();
+            //Pen pBlack = new Pen(Color.Gray, 1);
+            SolidBrush bBlack = new SolidBrush(Color.Black),
+                       bWhite = new SolidBrush(Color.White);
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    int x = i * res;
+                    int y = j * res;
+
+                    if (grid[i, j] == 1)
+                        graphics.FillRectangle(bBlack, new Rectangle(x, y, res - off, res - off));
+                    else
+                        graphics.FillRectangle(bWhite, new Rectangle(x, y, res - off, res - off));
+
+                    //graphics.DrawRectangle(pBlack, new Rectangle(x, y, res - off, res - off));
+                }
+            }
+
+            var newGrid = make2Darray(cols, rows);
+            // Compute newGrid based on grid
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    int state = grid[i, j];
+
+                    // Calculate neighbors
+                    var neighbors = countNeighs(grid, i, j, cols, rows);
+                    if (state == 0 && neighbors == 3)
+                        newGrid[i, j] = 1;
+                    else if (state == 1 && (neighbors < 2 || neighbors > 3))
+                        newGrid[i, j] = 0;
+                    else
+                        newGrid[i, j] = state;
+
+                }
+            }
+
+            grid = newGrid;
+
+            bBlack.Dispose();
+            bWhite.Dispose();
+            graphics.Dispose();
+        }
+
+        static int[,] populate(int[,] _2Darray)
+        {
+            
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    grid[i, j] = moreZero[GetRandom.Next(moreZero.Length)];
+                }
+            }
+            return _2Darray;
+        }
+
+        static int[,] zeroFill(int[,] _2Darray)
+        {
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    grid[i, j] = 0;
+                }
+            }
+            return _2Darray;
+        }
+
+        static int countNeighs(int[,] grid, int x, int y, int cols, int rows)
+        {
+            int sum = 0;
+            if (x == 0)
+            {
+                if (y == 0)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        for (int j = 0; j < 2; j++)
+                        {
+                            sum += grid[x + i, y + j];
+                        }
+                    }
+                }
+                else if (y == rows -1)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        for (int j = -1; j < 1; j++)
+                        {
+                            sum += grid[x + i, y + j];
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        for (int j = -1; j < 2; j++)
+                        {
+                            sum += grid[x + i, y + j];
+                        }
+                    }
+                }
+            }
+            else if (x == cols-1)
+            {
+                if (y == 0)
+                {
+                    for (int i = -1; i < 1; i++)
+                    {
+                        for (int j = 0; j < 2; j++)
+                        {
+                            sum += grid[x + i, y + j];
+                        }
+                    }
+                }
+                else if (y == rows -1)
+                {
+                    for (int i = -1; i < 1; i++)
+                    {
+                        for (int j = -1; j < 1; j++)
+                        {
+                            sum += grid[x + i, y + j];
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = -1; i < 1; i++)
+                    {
+                        for (int j = -1; j < 2; j++)
+                        {
+                            sum += grid[x + i, y + j];
+                        }
+                    }
+                }
+            }
+            else if (y == 0)
+            {
+                for (int i = -1; i < 2; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        sum += grid[x + i, y + j];
+                    }
+                }
+            }
+            else if (y == rows -1)
+            {
+                for (int i = -1; i < 2; i++)
+                {
+                    for (int j = -1; j < 1; j++)
+                    {
+                        sum += grid[x + i, y + j];
+                    }
+                }
+            }
+            else
+            {
+                for (int i = -1; i < 2; i++)
+                {
+                    for (int j = -1; j < 2; j++)
+                    {
+                        sum += grid[x + i, y + j];
+                    }
+                }
+            }
+
+            sum -= grid[x, y];
+            return sum;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            geneBox.Text = $"{generation++}";
+            beginGrowth(mainPanel);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            zeroFill(grid);
+            populate(grid);
+            updateDraw(mainPanel);
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            generation = 0;
+            startButton.Enabled = false;
+            button1.Enabled = false;
+            restartButton.Enabled = true;
+            timer1.Enabled = true;
+        }
+
+        private void geneBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                generation = 0;
+                zeroFill(grid);
+                timer1.Stop();
+                timer1.Enabled = false;
+                startButton.Enabled = true;
+                button1.Enabled = true;
+                restartButton.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = (int) numericUpDown1.Value * 10;
+        }
+
+        
+    }
+}
